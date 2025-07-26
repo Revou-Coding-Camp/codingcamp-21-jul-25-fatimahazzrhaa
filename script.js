9e3b58a04a49ee5d61490805a6244d16748fb5e8
fatimah azzahra <fatimahazzrhaa@gmail.com>
2025-07-26 21:17:03 +0700

    add innitial implementation of todo list application with HTML, CSS, and JavaScript

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log("")
+
+let tasks = [];
+
+function addTask() {
+
+    const taskInput = document.getElementById("todo-input");
+    const dateInput = document.getElementById("date-input");
+
+    if (taskInput.value === "" || dateInput.value === "") {
+        alert("Please enter task and date.");
+
+    } else {
+        tasks.push({
+        title: taskInput.value,
+        date: dateInput.value
+    });
+
+    console.log("Task added", taskInput.value, "on", dateInput.value);
+    console.log(tasks);
+
+    renderTasks();
+
+    }
+}
+
+function removeAllTask() {
+    
+    tasks = [];
+
+    renderTasks();
+
+}
+
+function toggleFilter() {
+
+}
+
+function renderTasks() {
+    const taskList = document.getElementById("todo-list");
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        taskList.innerHTML += `
+            <li class="todo-item flex justify-between items-center bg-white p-4 rounded-md mb-2">
+                <span>${task.title}</span>
+                <div>
+                    <button class="px-[26px] py-[4px] rounded-[8px] bg-green-500 text-white">Edit</button>
+                    <button class="px-[26px] py-[4px] rounded-[8px] bg-red-500 text-white">Delete</button>
+                </div>
+            </li>
+        `;
+    });
+}
